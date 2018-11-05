8118512a82039c65e02dd58855bb930066833684
celine kaslin <celine.kaslin@gmail.com>
2018-11-05 17:26:00 +0000

    more info about students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,29 @@
 def input_students
+
   puts "Please enter the students's names :"
-  puts "To finish, juts hit return twice"
-  students = Array.new
   name = gets.chomp
-  #while the name is not empty, repeat this code
-  while !name.empty? do
-    #add the student hash to the Array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    #gets an other name from the input_students
-    name = gets.chomp
+  puts "Please enter the student's birth place :"
+  birth_place = gets.chomp
+  puts "please enter the student's age :"
+  age = gets.chomp
+  puts "Please enter the student's hobby :"
+  hobby = gets.chomp
+  puts "To finish, juts enter 'stop'"
+
+  students = Array.new
+    while !name.empty? do
+      #add the student hash to the Array
+      students << {name: name,birth_place: birth_place, age: age, hobby: hobby, cohort: :november}
+      puts "Now we have #{students.count} students"
+      #gets an other name from the input_students
+      name = gets.chomp
+      if name == "stop"
+        break
+      end
+      birth_place = gets.chomp
+      age = gets.chomp
+      hobby = gets.chomp
   end
-  # return the array of students
   students
 end
 
@@ -21,10 +33,14 @@ def print_header
 end
 
 def print_name(names)
+  type_name = true
+  while type_name
     names.each_with_index do |student, index|
       index += 1
-      puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index} #{student[:name]}, #{student[:age]}, #{student[:hobby]} (#{student[:cohort]} cohort)"
     end
+    type_name = false
+  end
 end
 
 def print_footer(names)
