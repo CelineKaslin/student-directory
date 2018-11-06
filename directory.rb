339c1eb64cc795f90331a2706f1e6e42f1de4a02
celine kaslin <celine.kaslin@gmail.com>
2018-11-06 11:50:57 +0000

    default cohort improved

--- directory.rb
+++ directory.rb
@@ -2,26 +2,22 @@ def input_students
 
   puts "Please enter the students's names :"
   name = gets.chomp
-  puts "Please enter the student's cohort :"
-  cohort = gets.chomp
-  if cohort == ""
-    cohort = "November"
-  end
   puts "To finish, juts enter 'stop'"
 
   students = Array.new
     while !name.empty? do
+      puts "Please enter the student's cohort :"
+      cohort = gets.chomp
+      if cohort == ""
+         cohort = "November"
+       end
       #add the student hash to the Array
-      students << {name: name, cohort: cohort}
+      students << {name: name, cohort: cohort.to_sym}
       puts "Now we have #{students.count} students"
       #gets an other name from the input_students
       name = gets.chomp
       if name == "stop"
         break
-      end
-      cohort = gets.chomp
-      if cohort == ""
-        cohort = "November"
       end
   end
   students
@@ -33,13 +29,9 @@ def print_header
 end
 
 def print_name(names)
-  type_name = true
-  while type_name
     names.each_with_index do |student, index|
       index += 1
       puts "#{index}. #{student[:name].center(15)}, (#{student[:cohort]} cohort)"
-    end
-    type_name = false
   end
 end
 
