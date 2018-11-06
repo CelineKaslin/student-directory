885b48e590134c19697b36e9a80d6b5c177628c8
celine kaslin <celine.kaslin@gmail.com>
2018-11-06 16:43:53 +0000

    interactive menu refactored

--- directory.rb
+++ directory.rb
@@ -1,49 +1,60 @@
+@students = []
+
+def print_menu
+  puts "\nChoose one of the following action by typing the number of it :"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header if @students != []
+  print_students_list
+  print_footer
+end
+
+def case_action(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I do not know what you meant, try again"
+  end
+end
+
 def interactive_menu
-  students = []
   loop do
-    puts "\nChoose one of the following action by typing the number of it :"
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-  # do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print_name(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I do not know what you meant, try again"
-    end
+    print_menu
+    # do what the user has asked
+    case_action(gets.chomp) # user selection directly passed as argument
   end
 end
 
 def input_students
   puts "Please enter the students's names :"
   name = gets.tr("\n\r", "")
-  students = Array.new
-    while !name.empty? do
-      puts "Please enter the student's cohort :"
-      cohort = gets.chomp
-      puts "To finish, just enter 'stop'"
-      if cohort == ""
-         cohort = "November"
-       end
-      #add the student hash to the Array
-      students << {name: name, cohort: cohort.to_sym}
-      puts "Now we have #{students.count} student" if students.count == 1
-      puts "Now we have #{students.count} students" if students.count != 1
-      #gets an other name from the input_students
-      name = gets.chomp
-      if name == "stop"
-        break
-      end
+  while !name.empty? do
+    puts "Please enter the student's cohort :"
+    cohort = gets.chomp
+    puts "To finish, just enter 'stop'"
+    if cohort == ""
+       cohort = "November"
+     end
+    #add the student hash to the Array
+    @students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{@students.count} student" if @students.count == 1
+    puts "Now we have #{@students.count} students" if @students.count != 1
+    #gets an other name from the input_students
+    name = gets.chomp
+    if name == "stop"
+      break
+    end
   end
-  students
+  @students
 end
 
 def print_header
@@ -51,20 +62,20 @@ def print_header
   puts "--------------"
 end
 
-def print_name(students)
+def print_students_list
     cohorts = []
-    students.each {|student| cohorts.push(student[:cohort])}
+    @students.each {|student| cohorts.push(student[:cohort])}
     cohorts.uniq.each do |cohort|
       puts "Cohort: #{cohort}"
-      students.map do |student|
+      @students.map do |student|
         puts "#{student[:name].center(15)}" if student[:cohort] == cohort
       end
     end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great student" if students.count == 1
-  puts "Overall, we have #{students.count} great students" if students.count > 1
+def print_footer
+  puts "Overall, we have #{@students.count} great student" if @students.count == 1
+  puts "Overall, we have #{@students.count} great students" if @students.count > 1
 end
 
 # nothing happen until we call the methods
