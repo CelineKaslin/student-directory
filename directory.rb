77e1cd2614d35cb66f7aac3b1be7dae4fb856dbf
celine kaslin <celine.kaslin@gmail.com>
2018-11-08 10:23:13 +0000

    Save data in csv file

--- directory.rb
+++ directory.rb
@@ -1,9 +1,22 @@
 @students = []
 
+def save_students
+  # open the file for writting
+  file = File.open("students.csv", "w") # file name, write mode
+  # iterate over the array of save_students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line # write to the file
+  end
+  file.close
+end
+
 def print_menu
   puts "\nChoose one of the following action by typing the number of it :"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -19,6 +32,8 @@ def case_action(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
